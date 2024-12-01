956cd78b1525888b786fb348596ee9692c371410
Keshvi Kapadia <150351480+keshvi-kapadia@users.noreply.github.com>
2024-12-01 09:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+let turnO = true;
+let cnt = 0;
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let newbtn = document.querySelector(".new");
+let msg = document.querySelector(".msg");
+let p1 = prompt("Enter Player 1 name :");
+let p2 = prompt("Enter Player 2 name :");
+let winner = "";
+
+if (p1 == null) p1 = "O";
+if (p2 == null) p2 = "X";
+newbtn.classList.add("hide");
+
+let winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    cnt++;
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+};
+
+const enableBoxes = () => {
+  p1 = prompt("Enter Player 1 name :");
+  p2 = prompt("Enter Player 2 name :");
+  newbtn.classList.add("hide");
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+    cnt = 0;
+    msg.innerText = "";
+  });
+  turnO = true;
+};
+
+reset.addEventListener("click", enableBoxes);
+newbtn.addEventListener("click", enableBoxes);
+const checkWinner = () => {
+  if (cnt == 9) {
+    msg.innerText = "Game is Draw";
+    newbtn.classList.remove("hide");
+  } else {
+    for (patterns of winPatterns) {
+      val1 = boxes[patterns[0]].innerText;
+      val2 = boxes[patterns[1]].innerText;
+      val3 = boxes[patterns[2]].innerText;
+      if (val1 != "" && val2 != "" && val3 != "") {
+        if (val1 == val2 && val2 == val3) {
+          if (val1 == "O") winner = p1;
+          else winner = p2;
+
+          msg.innerText = `Congratulations!! player ${winner} won`;
+          newbtn.classList.remove("hide");
+          disableBoxes();
+        }
+      }
+    }
+  }
+};
